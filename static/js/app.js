13e661e2d316252a983c39ddce62cc3caab62ba2
Rahul <rahul.yadav@valuepitch.com>
2024-05-20 17:16:46 +0530

    Added Read More button

--- static/js/app.js
+++ static/js/app.js
@@ -12,3 +12,23 @@ const copyright = () => {
   tag.textContent = year;
 };
 copyright();
+
+// Mission
+const missionToggle = () => {
+  const cards = document.querySelectorAll(".mission .card");
+  cards.forEach((card) => {
+    const toggle = card.querySelector("b");
+    toggle.addEventListener("click", () => {
+      if (card.classList.contains("hide")) {
+        card.classList.remove("hide");
+        card.classList.add("show");
+        toggle.textContent = "Show Less";
+      } else {
+        card.classList.add("hide");
+        card.classList.remove("show");
+        toggle.textContent = "Read More";
+      }
+    });
+  });
+};
+missionToggle();
